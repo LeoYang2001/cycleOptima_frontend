0bc9967b839cb418e3be7471e30cc347ffcea219
LeoYang2001 <jya261@uky.edu>
2025-07-17 10:57:11 -0400

    ticks finished

--- src/components/phaseEditor/ComponentTimeline.tsx
+++ src/components/phaseEditor/ComponentTimeline.tsx
@@ -3,6 +3,7 @@ import type { CycleComponent } from "../../types/common/CycleComponent";
 import { useDroppable } from "@dnd-kit/core";
 import { Plus } from "lucide-react";
 import { getStyle } from "./ComponentLibrary";
+import { generateTicksWithStartTime } from "../../utils/generateTicks";
 
 interface ComponentViewProps {
   component: CycleComponent;
@@ -113,6 +114,12 @@ function ComponentTimeline({
   };
 
   const percentageData = calculatePercentages();
+  const ticks = generateTicksWithStartTime(
+    Number(percentageData.totalPhaseDuration),
+    Number(percentageData.pauseTime)
+  );
+
+  console.log("ticks:", ticks);
 
   let maskStyle = {
     zIndex: -1,
@@ -136,8 +143,8 @@ function ComponentTimeline({
   }
 
   return (
-    <div ref={setNodeRef} className=" w-full h-full flex flex-col ">
-      <div className="mb-4 p-3 bg-gray-800 rounded-lg text-white text-sm">
+    <div ref={setNodeRef} className=" w-full h-full flex flex-col pt-10 ">
+      {/* <div className="mb-4 p-3 bg-gray-800 rounded-lg text-white text-sm">
         <div className="grid grid-cols-2 gap-4 mb-2">
           <div>
             <span className="text-gray-400">Total Phase Duration: </span>
@@ -170,15 +177,26 @@ function ComponentTimeline({
             </span>
           </div>
         </div>
-      </div>
-      <div style={{}} className="w-full h-full rounded-lg relative ">
+      </div> */}
+      <div style={{}} className="w-full h-full  rounded-lg relative ">
+        {components.length === 0 && !isDragging && (
+          <div
+            style={{
+              border: "1px dashed #333",
+              borderRadius: "8px",
+            }}
+            className="w-full h-full flex items-center absolute justify-center text-lg font-semibold text-gray-500"
+          >
+            No components to display
+          </div>
+        )}
         {isDragging && (
           <div
             style={{
               border: "1px dashed #3673d9",
               borderRadius: "8px",
             }}
-            className="absolute w-full h-full flex justify-center items-center"
+            className="absolute z-10 w-full h-full flex justify-center items-center"
           >
             {isOver ? (
               <span
@@ -209,16 +227,105 @@ function ComponentTimeline({
             borderRadius: "8px",
             ...maskStyle,
           }}
-          className=" transition-all flex  items-center duration-200 absolute w-full h-full  left-0 top-0 flex-col justify-start"
+          className=" transition-all  z-10 flex  items-center duration-200 absolute w-full h-full  left-0 top-0 flex-col justify-start"
         ></div>
+        {/* BACKGROUND TICKS  */}
+        {components.length > 0 && (
+          <div
+            style={{
+              backgroundColor: "#18181b",
+              borderWidth: 1,
+              borderRadius: "8px",
+              border: "1px solid #333",
+            }}
+            className="absolute w-full h-full flex flex-row z-1"
+          >
+            <div
+              className="   bg-transparent border-r-1 h-full"
+              style={{
+                width: `${Math.min(
+                  percentageData.pauseTimePercentage,
+                  35
+                ).toFixed(1)}%`,
+                borderColor: "#333",
+                backgroundImage:
+                  "repeating-linear-gradient(45deg, transparent, transparent 5px, rgba(255,255,255,0.1) 5px, rgba(255,255,255,0.1) 10px)",
+              }}
+            >
+              <span
+                style={{
+                  left: 0,
+                  top: -35,
+                  transform: "translateX(-50%)",
+                }}
+                className=" absolute  text-sm text-gray-500"
+              >
+                {ticks[0].label}
+              </span>
+            </div>
+            <div className=" flex-1 flex h-full bg-transparent   flex-row ">
+              {ticks.slice(1, -1).map((tick, index) => {
+                if (index === ticks.length - 3) {
+                  return (
+                    <div
+                      key={index}
+                      style={{
+                        borderColor: "#333",
+                      }}
+                      className="flex-1 flex h-full bg-transparent border-r-1  flex-row relative"
+                    >
+                      <span
+                        style={{
+                          top: -35,
+                          transform: "translateX(-50%)",
+                        }}
+                        className=" absolute  text-sm text-gray-500"
+                      >
+                        {tick.label}
+                      </span>
+                      <span
+                        style={{
+                          top: -35,
+                          right: 0,
+                          transform: "translateX(50%)",
+                        }}
+                        className=" absolute  text-sm text-gray-500"
+                      >
+                        {ticks[ticks.length - 1].label}
+                      </span>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      key={index}
+                      style={{
+                        borderColor: "#333",
+                      }}
+                      className="flex-1 flex h-full bg-transparent  border-r-1 flex-row relative"
+                    >
+                      <span
+                        style={{
+                          top: -35,
+                          transform: "translateX(-50%)",
+                        }}
+                        className=" absolute  text-sm text-gray-500"
+                      >
+                        {tick.label}
+                      </span>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </div>
+        )}
+        {/* COMPONENTS TIMELINE VIEW  */}
         <div
           style={{
-            backgroundColor: "#18181b",
-            borderWidth: 1,
-            borderRadius: "8px",
-            border: "1px solid #333",
+            backgroundColor: "transparent",
           }}
-          className="border border-white  overflow-x-hidden items-center h-full flex flex-row p-2 overflow-y-scroll custom-scrollbar "
+          className=" z-10 overflow-x-hidden items-center h-full flex flex-row overflow-y-scroll custom-scrollbar relative"
         >
           <div
             style={{
@@ -227,13 +334,9 @@ function ComponentTimeline({
                 35
               ).toFixed(1)}%`,
             }}
-            className=" border-b-2 border-white flex items-center justify-center text-xs font-semibold"
-          >
-            {percentageData.pauseTimePercentage > 35
-              ? `${percentageData.pauseTimePercentage.toFixed(1)}%`
-              : "pauseTime"}
-          </div>
-          <div className="flex-1 h-full border border-white p-2">
+            className=" flex items-center justify-center text-xs font-semibold"
+          ></div>
+          <div className="flex-1 h-full p-2">
             {percentageData.componentPercentages.map((componentData) => {
               const component = components.find(
                 (c) => c.id === componentData.id
